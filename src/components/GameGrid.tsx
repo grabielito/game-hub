432a7e5d9d6e69d395a1a08e213734fee9db5e0f
gabo <gaboramirezdev@gmail.com>
2024-06-09 11:11:41 -0500

    Create_useGame

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,28 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGames {
-  count: number;
-  results: Game[];
-}
+import useGame from "../hooks/useGame";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, seterror] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGames>("/games")
-      .then((res) => setGames([...res.data.results]))
-      .catch((err) => seterror(err.message));
-  }, []);
-
+  const { error, games, isLoading } = useGame();
   return (
     <div>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useGame.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+  }
+  
+  interface FetchGames {
+    count: number;
+    results: Game[];
+  }
+
+const useGame = () => {
+    const [games, setgames] = useState<Game[]>([]);
+    const [error, seterror] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+      const controller= new AbortController();
+        setIsLoading(true);
+        apiClient
+          .get<FetchGames>("/games",{signal:controller.signal})
+          .then((res) => {setgames([...res.data.results]);
+            setIsLoading(false);
+          })
+          .catch((err) => {
+            if (err instanceof CanceledError) return;
+            
+            seterror(err.message);
+            setIsLoading(false);
+          });
+
+          return ()=>{controller.abort()}
+      }, []);
+  return {games,error,isLoading};
+}
+
+export default useGame;
