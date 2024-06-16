7fb8c1e6046946c8f73694e0d4ffebde1384e405
gabo <gaboramirezdev@gmail.com>
2024-06-16 09:41:27 -0500

    Displaying Icons

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react"
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform{
+  id:number;
+  name:string;
+  slug:string
+}
 export interface Game {
     id: number;
     name: string;
     background_image:string;
+    parent_platforms:{platform:Platform}[]
   }
   
   interface FetchGames {
@@ -25,7 +31,7 @@ const useGame = () => {
           .get<FetchGames>("/games",{signal:controller.signal})
           .then((res) => {setgames([...res.data.results]);
             setIsLoading(false);
-          })
+            })
           .catch((err) => {
             if (err instanceof CanceledError) return;
 
